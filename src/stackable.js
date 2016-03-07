28ac1068eef2a584ea7d7c007eac020bb766b33a
Parminder Klair <p.klair@scorchsoft.com>
2016-03-07 17:33:49 +0000

    post items init

--- src/stackable.js
+++ src/stackable.js
@@ -35,6 +35,18 @@ class Stackable {
     });
   }
 
+  createItem(containerId, data, callback) {
+    this._post(`items`, {containerId}, data, function(err, res) {
+      callback(err, res);
+    });
+  }
+
+  updateItem(itemId, data, callback) {
+    this._put(`items/${itemId}`, data, function(err, res) {
+      callback(err, res);
+    });
+  }
+
   _get(path, callback) {
     let endPoint = `${this._apiUrl}/${this._apiVersion}/${path}?token=${this._token}`;
 
@@ -58,7 +70,92 @@ class Stackable {
       //is browser
       $.ajax({
         url: endPoint,
+        type: 'GET',
         context: document.body,
+        success: function(response) {
+          callback(false, response);
+        },
+        error: function(err) {
+          callback(err, false);
+        }
+      });
+    }
+  }
+
+  _post(path, data, callback) {
+    let endPoint = `${this._apiUrl}/${this._apiVersion}/${path}?token=${this._token}`;
+
+    if (typeof window === 'undefined') {
+      //is node //todo
+      fetch(endPoint, {method: 'POST', body: data})
+        .then(function(response) {
+          if (response.status >= 400) {
+            let err = {
+              'message': 'There was an error with this request.'
+            };
+            return callback(err, false);
+          }
+
+          return response.json();
+        })
+        .then(function(response) {
+          return callback(false, response);
+        });
+    } else {
+      //is browser
+      $.ajax({
+        url: endPoint,
+        type: 'POST',
+        context: document.body,
+        data: data,
+        success: function(response) {
+          callback(false, response);
+        },
+        error: function(err) {
+          callback(err, false);
+        }
+      });
+    }
+  }
+
+  _put(path, params, data, callback) {
+    let endPoint = `${this._apiUrl}/${this._apiVersion}/${path}?token=${this._token}`;
+
+    var paramsStr = "";
+    for (var key in params) {
+      if (paramsStr != "") {
+        paramsStr += "&";
+      }
+      paramsStr += key + "=" + encodeURIComponent(params[key]);
+    }
+
+    if (paramsStr.length > 0) {
+      endPoint = endPoint + '&' + paramsStr;
+    }
+
+    if (typeof window === 'undefined') {
+      //is node
+      fetch(endPoint, {method: 'PUT', body: data})
+        .then(function(response) {
+          if (response.status >= 400) {
+            let err = {
+              'message': 'There was an error with this request.'
+            };
+            return callback(err, false);
+          }
+
+          return response.json();
+        })
+        .then(function(response) {
+          return callback(false, response);
+        });
+    } else {
+      //is browser
+      $.ajax({
+        url: endPoint,
+        type: 'PUT',
+        context: document.body,
+        data: data,
         success: function(response) {
           callback(false, response);
         },
